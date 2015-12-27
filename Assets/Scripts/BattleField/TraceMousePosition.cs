7ee1251123a39465f2ec88b6e371793279812c70
yagamiiori <darkbear21@hotmail.co.jp>
2015-12-28 04:09:27 +0900

    ・カーソル追随クラスを実装

---
+++ Assets/Scripts/BattleField/TraceMousePosition.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;   // コレクションクラスの定義に必要
+using System.Linq;
+
+/// <summary>
+/// カーソル追随クラス
+/// <para>　カーソルが画面端に行った時、カーソルをカメラが追随する。</para>
+/// <para>　アタッチGO：メインカメラ</para>
+/// </summary>
+public class TraceMousePosition : MonoBehaviour
+{
+    /// <summary>
+    /// カメラがカーソルに追随する速度
+    /// </summary>
+    private float cameraSpeed = 0.0002f;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    private TraceMousePosition() { }
+
+    void Start()
+    {
+        Vector3 t = new Vector3(3.56f, 6.25f, -8.24f);
+        this.transform.position = t;
+    }
+
+    void Update()
+    {
+        // マウスのワールド座標をビューポート座標に変換
+        var t = Camera.main.WorldToViewportPoint(Input.mousePosition);
+
+//        Debug.Log(t);
+
+        var setPos = Vector3.Lerp(this.transform.position, t, cameraSpeed);
+
+        // 下方向
+        if (6.0f > t.y)
+        {
+            // 左下
+            if (13.0f > t.x)
+            {
+                setPos.x -= 0.2f;
+                setPos.y -= 0.2f;
+                this.transform.position = setPos;
+            }
+            // 右下
+            else if (46.0f < t.x)
+            {
+                setPos.x += 0.2f;
+                setPos.y -= 0.2f;
+                this.transform.position = setPos;
+            }
+            // 真下
+            else
+            {
+                setPos.x = this.transform.position.x;
+                setPos.y -= 0.2f;
+                this.transform.position = setPos;
+            }
+        }
+        // 上方向
+        else if (48.0f < t.y)
+        {
+            // 左上
+            if (13.0f > t.x)
+            {
+                setPos.x -= 0.2f;
+                setPos.y += 0.2f;
+                this.transform.position = setPos;
+            }
+            // 右上
+            else if (46.0f < t.x)
+            {
+                setPos.x += 0.2f;
+                setPos.y += 0.2f;
+                this.transform.position = setPos;
+            }
+            // 真上
+            else
+            {
+                setPos.x = this.transform.position.x;
+                setPos.y += 0.2f;
+                this.transform.position = setPos;
+            }
+        }
+        // 左右
+        else
+        {
+            // 左
+            if (6.0f > t.x)
+            {
+                setPos.x -= 0.2f;
+                setPos.y = this.transform.position.y;
+                this.transform.position = setPos;
+            }
+            // 右
+            else if (48.0f < t.x)
+            {
+                setPos.x += 0.2f;
+                setPos.y = this.transform.position.y;
+                this.transform.position = setPos;
+            }
+            else
+            {
+                // 中央付近は何もしない
+            }
+        }
+    }
+}
